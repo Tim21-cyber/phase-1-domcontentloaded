2893f48e0a0dfab798f89cfb38e1247c28f0d5b5
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-06-15 18:58:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.querySelector('p').textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('p').textContent = "This is really cool!";
+    }
+);
